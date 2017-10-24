ae7964b024775eae6245799fa67ae19a4208d697
ULINEDM\andrew.rohrer <andrew.rohrer@uline.com>
2017-10-24 10:22:40 -0500

    Add Javascript function to populate hours drop down

--- your-first-pwapp-master/Top3PWA/js/scripts.js
+++ your-first-pwapp-master/Top3PWA/js/scripts.js
@@ -1,6 +1,7 @@
 function popSubCategory() {
     var jsonFileName = "";
-    switch ($('')) {
+    $('#mainCategory').empty();
+    switch ($('#mainCategory').find(":selected").text()) {
         case "Enhancement":
             jsonFileName = "Enhcategory.json";
             break;
@@ -37,3 +38,19 @@ function popSubCategory() {
         }
     });
 }
+
+$(document).ready(fuction(){
+    var hourList = ["0.25", "0.5", "0.75", "1.0", 
+                    "1.25", "1.5", "1.75", "2.0", 
+                    "2.25", "2.5", "2.75", "3.0", 
+                    "3.25", "3.5", "3.75", "4.0",
+                    "4.25", "4.5", "4.75", "5.0",
+                    "5.25", "5.5", "5.75", "6.0",
+                    "6.25", "6.5", "6.75", "7.0",
+                    "7.25", "7.5", "7.75", "8.0"];
+    
+    for (var i = 0; i < hourList.length; i++){
+        $('#subcatselect').append(hourList[i]);
+    }
+
+});
