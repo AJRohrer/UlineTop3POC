688265523ee99e97321743b16dd715d0140f864f
Charles Pechous <Charles.Pechous@uline.com>
2017-10-26 09:52:07 -0500

    Add json to Top3List

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+var fs = require("fs");
 
 /* serves main page */
 app.get("/", function (req, res) {
@@ -10,7 +11,31 @@ app.get("/", function (req, res) {
 app.post("/user/add", function (req, res) {
     var top3Info = req.body;
     console.log(top3Info)
-    res.send("OK");
+
+    var obj = new Object();
+    obj = {
+        Top3s : []
+    };
+
+    var json = JSON.stringify(obj);
+
+    fs.readFile('./data/Top3List.json', function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            obj = JSON.parse(data);
+            obj.Top3s.push({ id: 2, square: 3 });
+            json = JSON.stringify(obj);
+            fs.writeFile('./data/Top3List.json', json, 'utf8', function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("JSON successfully appended");
+                }
+            });
+        }
+    });
+    res.send(json);
 });
 
 /* serves all the static files */
